a52afdf44690746e3c2b37b6ea3e56d64e2bbda3
eric <eric@mail.com>
2018-03-10 16:48:28 +0700

    models relation specified

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Item;
 
 class Category extends Model
 {
   protected $table="categories";
   protected $fillable=["name"];
   protected $guarded=[];
+
+  public function cats(){
+    return $this->hasMany("Apps\Model\Item");
+  }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Item extends Model
 {
   protected $table="items";
   protected $fillable=["category_id","name","price","stock"];
   protected $guarded=[];
+
+  public function cats(){
+    return $this->belongsTo("App\Models\Category", "category_id");
+  }
 }
